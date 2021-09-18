54d6f2803cb82821ed93e41d9f3ef0194ec1054a
Fauzan Rifki Maulana <80396521+frmdeveloper@users.noreply.github.com>
2021-09-18 10:26:12 +0700

    update w10x64.sh

--- w10x64.sh
+++ w10x64.sh
@@ -1,14 +1,3 @@
-echo "Download windows files"
-wget -O w10x64.img https://bit.ly/akuhnetW10x64
-echo "Download ngrok"
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-./ngrok authtoken 1p0oQSuvZdvucysO6OXmQVAPRFB_5Mc9VZDuQYFMRtf42fVCv
-nohup ./ngrok tcp 3388 &>/dev/null &
-echo "Wait"
-echo "Starting Windows"
-qemu-system-x86_64 -hda w10x64.img -m 4G -smp cores=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
-clear
 echo RDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
