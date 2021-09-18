c6dca18221698ecba2e66cd88f41f643573ec7e6
Fauzan Rifki Maulana <80396521+frmdeveloper@users.noreply.github.com>
2021-09-18 10:12:28 +0700

    update w10x64.sh

--- w10x64.sh
+++ w10x64.sh
@@ -6,8 +6,6 @@ wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/nu
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 ./ngrok authtoken 1p0oQSuvZdvucysO6OXmQVAPRFB_5Mc9VZDuQYFMRtf42fVCv
 nohup ./ngrok tcp 3388 &>/dev/null &
-echo Downloading File From akuh.net
-apt-get install qemu-system-x86 -y
 echo "Wait"
 echo "Starting Windows"
 qemu-system-x86_64 -hda w10x64.img -m 4G -smp cores=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
